50122a69fe7b00c76149ad45b7f9e12b855e83d8
artem.mardakhaev2004 <artem.mardakhaev2004@gmail.com>
2025-06-18 12:42:22 -0400

    Migration for sync service

---
+++ SyncService/Migrations/20250618164126_Initial.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyncService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TasksSyncMapping",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TaskId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CalendarEventId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TasksSyncMapping", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersSyncState",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    GoogleAccessToken = table.Column<string>(type: "text", nullable: false),
+                    GoogleRefreshToken = table.Column<string>(type: "text", nullable: false),
+                    TokenExpiry = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersSyncState", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TasksSyncMapping");
+
+            migrationBuilder.DropTable(
+                name: "UsersSyncState");
+        }
+    }
+}
